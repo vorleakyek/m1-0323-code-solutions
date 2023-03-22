134476228becccacada93a51dc7914f69e6a0b41
Vorleak Yek <vorleakyek@gmail.com>
2023-03-22 02:48:24 +0000

    Complete javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/javascript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,52 @@
+# javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What data type is returned by an arithmetic operation?
+A: a number
+
+- What is string concatenation?
+A: combining strings together into one string
+
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+A: There are 2 purposes:
+Arithmetic operation - adding numbers
+String concatination - joining string together
+
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+A: a boolean value (true or false)
+
+- What does the `+=` "plus-equals" operator do?
+A: the addition assignment operator
+
+It adds the value of the right operand to a variable and assigns the result to the variable.
+
+Example: x += y is the same as x = x +y;
+
+## Notes
+
+All student notes should be written here.
+
+
+How to write `Code Examples` in markdown
+
+for JS:
+```javascript
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const width = 5;
+const height = 7;
+const area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 3;
+const payment = 5;
+const change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+const quizzes = 55;
+const midterm = 85;
+const final = 97;
+const grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Billy';
+const lastName = 'Bill';
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 9;
+const isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 300;
+const isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
