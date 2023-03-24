8d554e874a350ab21c129432030e7d0dc70e87dc
Vorleak Yek <vorleakyek@gmail.com>
2023-03-24 03:18:19 +0000

    Complete javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,71 @@
+const orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    totalPrice: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114 - 3941689 - 8772232',
+    deliveredDate: 'Aug 8, 2020',
+    'Specific Instruction': null,
+    item: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Raushmayer, Dr.Axel',
+        returnDueDate: 'Sep 7, 2020',
+        price: 31.55
+      }
+    ]
+  },
+  {
+    orderDate: 'July 19, 2020',
+    totalPrice: 43.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113 - 3941689 - 8772232',
+    deliveredDate: 'July 20, 2020',
+    'Specific Instruction': 'Your package was delivered. It was handed directly to a resident.',
+    item: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnDueDate: 'Aug 19, 2020',
+        price: 41.33
+      }
+    ]
+  },
+  {
+    orderDate: 'July 4, 2020',
+    totalPrice: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114 - 3941689 - 8772232',
+    deliveredDate: 'Jul 7, 2020',
+    'Specific Instruction': 'Your package was delivered. It was handed directly to a resident.',
+    item: [
+      {
+        title: 'Gamecube controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-GameCube Adapter',
+        returnDueDate: 'Aug 5, 2020',
+        price: 15.98
+      }
+    ]
+  },
+  {
+    orderDate: 'July 3, 2020',
+    totalPrice: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113 - 3941689 - 8772232',
+    deliveredDate: 'Jul 5, 2020',
+    'Specific Instruction': null,
+    item: [
+      {
+        title: 'Gamecube controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnDueDate: 'Aug 4, 2020',
+        price: 94.95
+      },
+      {
+        title: 'The Art of Sql)',
+        author: 'Faroult, Stephane',
+        returnDueDate: 'Aug 4, 2020',
+        price: 33.99
+      }
+    ]
+  }
+
+];
