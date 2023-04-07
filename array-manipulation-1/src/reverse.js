951823a0da670fd20a4cb729c16dee8b8b0baab3
Vorleak Yek <vorleakyek@gmail.com>
2023-04-07 03:41:47 +0000

    Complete array-manipulation-1 exercise.

---
+++ array-manipulation-1/src/reverse.js
@@ -0,0 +1,8 @@
+/* exported reverse */
+function reverse(array) {
+  const newArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
---
+++ array-manipulation-1/src/tail.js
@@ -0,0 +1,8 @@
+/* exported tail */
+function tail(array) {
+  const newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
