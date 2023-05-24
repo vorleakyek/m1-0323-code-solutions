018fa84d2b19ca8c9767b6986b0e3ee61197e7b1
Vorleak Yek <vorleakyek@gmail.com>
2023-05-24 03:16:47 +0000

    Complete javascript-local-storage exercise.

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,14 @@
+/* exported todos */
+
+let todos = [];
+const previousTodosJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+
+window.addEventListener('beforeunload', handler);
+function handler(event) {
+  const todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
